dbdf0759ccad82c4527d9710f5f77cb8da559350
heathernfran <heathernfran@gmail.com>
2023-11-28 13:00:46 -0800

    Set up a11y with eslint and react-axe

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (!import.meta.env.PROD) {
+  import('react-axe').then((axe) => {
+    axe.default(React, ReactDOM, 1000)
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
