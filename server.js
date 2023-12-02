4fd6c1331fd11040757d241b8e386c8e880bf744
heathernfran <heathernfran@gmail.com>
2023-12-02 15:29:11 -0800

    Replace js data file with json

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+import fs from 'node:fs'
 import express from 'express'
 import ViteExpress from 'vite-express'
-import { data } from './src/data/users.js'
 
 const app = express()
 const port = 3000
+
+const filePath = './src/data/users.json'
+const fileContent = fs.readFileSync(filePath, 'utf-8')
+const data = JSON.parse(fileContent)
 
 app.get('/message', (_req, res) => {
   res.send('Hello from Express!')
